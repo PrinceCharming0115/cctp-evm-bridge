b0c427b723bb5764641394a1f40e42ef2885633e
bd21 <bd21@uw.edu>
2023-08-25 16:46:37 -0700

    checkpoint

--- src/TokenMessengerWithMetadata.sol
+++ src/TokenMessengerWithMetadata.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.7.6;
 
-import "evm-cctp-contracts/src/interfaces/IMessageTransmitter.sol";
+import "evm-cctp-contracts/src/MessageTransmitter.sol";
 import "evm-cctp-contracts/src/interfaces/IMintBurnToken.sol";
 import "evm-cctp-contracts/src/messages/Message.sol";
 import "evm-cctp-contracts/src/TokenMessenger.sol";
@@ -17,39 +17,123 @@ contract TokenMessengerWithMetadata {
     event DepositForBurnMetadata(
         uint64 indexed nonce, uint64 indexed metadataNonce, bytes metadata
     );
+    event Collect(
+        address indexed burnToken, 
+        bytes32 mintRecipient, 
+        uint256 amountBurned, 
+        uint256 fee,
+        uint32 source, 
+        uint32 dest
+    );
+    // TODO
+    event Debug(string tag, uint256 amount);
 
     // ============ State Variables ============
     TokenMessenger public tokenMessenger;
-    IMessageTransmitter public immutable messageTransmitter;
+    MessageTransmitter public immutable messageTransmitter;
 
     uint32 public immutable domainNumber;
     bytes32 public immutable domainRecipient;
+
+    // the address that can update parameters
+    address public owner;
+    // the address where fees are sent
+    address payable public collector; 
+
+    struct Fee {
+        // percentage fee in bips
+        uint256 percFee;
+        // flat fee in uusdc (1 uusdc = 10^-6 usdc)
+        uint256 flatFee;
+        // needed for null check
+        bool isInitialized;
+    }
+    
+    // mapping of destination domain -> fee
+    mapping(uint32 => Fee) public feeMap;
+
+    // the domain id this contract is deployed on
+    uint32 public immutable domain;
 
     // ============ Constructor ============
     /**
      * @param _tokenMessenger Token messenger address
      * @param _domainNumber Noble's domain number
      * @param _domainRecipient Noble's domain recipient
+     * @param _domain The domain id this contract is deployed on
      */
     constructor(
         address _tokenMessenger,
         uint32 _domainNumber,
-        bytes32 _domainRecipient
+        bytes32 _domainRecipient,
+        uint32 _domain,
+        address payable _collector
     ) {
         require(_tokenMessenger != address(0), "TokenMessenger not set");
         tokenMessenger = TokenMessenger(_tokenMessenger);
-        messageTransmitter = tokenMessenger.localMessageTransmitter();
+        messageTransmitter = MessageTransmitter(
+            address(tokenMessenger.localMessageTransmitter())
+        );
 
         domainNumber = _domainNumber;
         domainRecipient = _domainRecipient;
+        domain = _domain;
+        collector = _collector;
+        owner = msg.sender;
     }
 
     // ============ External Functions ============
+    /**
+     * @notice Wrapper function.
+     * If destinationCaller is empty, call "depositForBurnWithCaller", otherwise call "depositForBurn"
+     * 
+     * @param amount - the burn amount
+     * @param destinationDomain - domain id the funds will be minted on
+     * @param mintRecipient - address receiving minted tokens on destination domain
+     * @param burnToken - address of the token being burned on the source chain
+     * @param destinationCaller - address allowed to mint on destination chain
+     */
+    function depositForBurn(
+        uint256 amount,
+        uint32 destinationDomain,
+        bytes32 mintRecipient,
+        address burnToken,
+        bytes32 destinationCaller
+    ) external {
+
+        IMintBurnToken token = IMintBurnToken(burnToken);
+        token.transferFrom(msg.sender, address(this), amount);
+        token.approve(address(tokenMessenger), amount);
+
+        // collect fee
+        uint256 fee = calculateFee(amount, domainNumber);
+        token.transfer(collector, fee);
+
+        if (destinationCaller == bytes32(0)) {
+            tokenMessenger.depositForBurn(
+                amount - fee, 
+                destinationDomain, 
+                mintRecipient, 
+                burnToken
+            );
+        } else {
+            tokenMessenger.depositForBurnWithCaller(
+                amount - fee, 
+                destinationDomain, 
+                mintRecipient, 
+                burnToken,
+                destinationCaller
+            );
+        }
+        emit Collect(burnToken, mintRecipient, amount-fee, fee, domain, domainNumber);
+    }
+
     /**
      * @notice Wrapper function for "depositForBurn" that includes metadata.
      * Emits a `DepositForBurnMetadata` event.
      * @param channel channel id to be used when ibc forwarding
      * @param destinationRecipient address of recipient once ibc forwarded
+     * @param destinationBech32Prefix bech32 prefix used for address encoding once ibc forwarded
      * @param amount amount of tokens to burn
      * @param mintRecipient address of mint recipient on destination domain
      * @param burnToken address of contract to burn deposited tokens, on local domain
@@ -58,14 +142,23 @@ contract TokenMessengerWithMetadata {
      */
     function depositForBurn(
         uint64 channel,
+        bytes32 destinationBech32Prefix,
         bytes32 destinationRecipient,
         uint256 amount,
         bytes32 mintRecipient,
         address burnToken,
         bytes calldata memo
     ) external returns (uint64 nonce) {
-        bytes memory metadata =
-            abi.encodePacked(channel, destinationRecipient, memo);
+        uint64 reservedNonce = messageTransmitter.nextAvailableNonce();
+        bytes32 sender = Message.addressToBytes32(msg.sender);
+        bytes memory metadata = abi.encodePacked(
+            reservedNonce,
+            sender,
+            channel,
+            destinationBech32Prefix,
+            destinationRecipient,
+            memo
+        );
 
         return rawDepositForBurn(amount, mintRecipient, burnToken, metadata);
     }
@@ -89,16 +182,19 @@ contract TokenMessengerWithMetadata {
         token.transferFrom(msg.sender, address(this), amount);
         token.approve(address(tokenMessenger), amount);
 
+        // collect fee
+        uint256 fee = calculateFee(amount, domainNumber);
+        token.transfer(collector, fee);  
+
         nonce = tokenMessenger.depositForBurn(
-            amount, domainNumber, mintRecipient, burnToken
+            amount-fee, domainNumber, mintRecipient, burnToken
         );
 
-        bytes32 sender = Message.addressToBytes32(msg.sender);
-        bytes memory message = abi.encodePacked(nonce, sender, metadata);
         uint64 metadataNonce = messageTransmitter.sendMessage(
-            domainNumber, domainRecipient, message
+            domainNumber, domainRecipient, metadata
         );
 
+        emit Collect(burnToken, mintRecipient, amount-fee, fee, domain, domainNumber);
         emit DepositForBurnMetadata(nonce, metadataNonce, metadata);
     }
 
@@ -107,6 +203,7 @@ contract TokenMessengerWithMetadata {
      * Emits a `DepositForBurnMetadata` event.
      * @param channel channel id to be used when ibc forwarding
      * @param destinationRecipient address of recipient once ibc forwarded
+     * @param destinationBech32Prefix bech32 prefix used for address encoding once ibc forwarded
      * @param amount amount of tokens to burn
      * @param mintRecipient address of mint recipient on destination domain
      * @param burnToken address of contract to burn deposited tokens, on local domain
@@ -116,6 +213,7 @@ contract TokenMessengerWithMetadata {
      */
     function depositForBurnWithCaller(
         uint64 channel,
+        bytes32 destinationBech32Prefix,
         bytes32 destinationRecipient,
         uint256 amount,
         bytes32 mintRecipient,
@@ -123,8 +221,16 @@ contract TokenMessengerWithMetadata {
         bytes32 destinationCaller,
         bytes calldata memo
     ) external returns (uint64 nonce) {
-        bytes memory metadata =
-            abi.encodePacked(channel, destinationRecipient, memo);
+        uint64 reservedNonce = messageTransmitter.nextAvailableNonce();
+        bytes32 sender = Message.addressToBytes32(msg.sender);
+        bytes memory metadata = abi.encodePacked(
+            reservedNonce,
+            sender,
+            channel,
+            destinationBech32Prefix,
+            destinationRecipient,
+            memo
+        );
 
         return rawDepositForBurnWithCaller(
             amount, mintRecipient, burnToken, destinationCaller, metadata
@@ -152,16 +258,43 @@ contract TokenMessengerWithMetadata {
         token.transferFrom(msg.sender, address(this), amount);
         token.approve(address(tokenMessenger), amount);
 
+        // collect fee
+        uint256 fee = calculateFee(amount, domainNumber);
+        emit Debug("amount", amount);
+        emit Debug("fee", fee);
+        token.transfer(collector, fee);
+        emit Collect(burnToken, mintRecipient, amount-fee, fee, domain, domainNumber);
+
         nonce = tokenMessenger.depositForBurnWithCaller(
             amount, domainNumber, mintRecipient, burnToken, destinationCaller
         );
 
-        bytes32 sender = Message.addressToBytes32(msg.sender);
-        bytes memory message = abi.encodePacked(nonce, sender, metadata);
         uint64 metadataNonce = messageTransmitter.sendMessageWithCaller(
-            domainNumber, domainRecipient, destinationCaller, message
+            domainNumber, domainRecipient, destinationCaller, metadata
         );
 
+        emit Collect(burnToken, mintRecipient, amount-fee, fee, domain, domainNumber);
         emit DepositForBurnMetadata(nonce, metadataNonce, metadata);
+    }
+
+    function calculateFee(uint256 amount, uint32 destinationDomain) private view returns (uint256) {
+        Fee memory fee = feeMap[destinationDomain];
+        require(fee.isInitialized, "Fee not found.");
+        return (amount * fee.percFee) / 10000 + fee.flatFee;
+    }
+
+    function setFee(uint32 destinationDomain, uint256 percFee, uint256 flatFee) external {
+        require(msg.sender == owner, "Only the owner can update fees");
+        feeMap[destinationDomain] = Fee(percFee, flatFee, true);
+    }
+
+    function updateOwner(address newOwner) external {
+        require(msg.sender == owner, "Only owner can update owner");
+        owner = newOwner;
+    }
+
+    function updateCollector(address payable newCollector) external {
+        require(msg.sender == owner, "Only owner can update collector");
+        collector = newCollector;
     }
 }
--- test/TokenMessengerWithMetadata.t.sol
+++ test/TokenMessengerWithMetadata.t.sol
@@ -6,8 +6,20 @@ import "evm-cctp-contracts/src/messages/BurnMessage.sol";
 import "evm-cctp-contracts/src/MessageTransmitter.sol";
 import "evm-cctp-contracts/test/TestUtils.sol";
 import "../src/TokenMessengerWithMetadata.sol";
+import "../lib/forge-std/src/Test.sol";
 
 contract TokenMessengerWithMetadataTest is Test, TestUtils {
+    // ============ Events ============
+    event Collect(
+        address indexed burnToken, 
+        bytes32 mintRecipient, 
+        uint256 amountBurned, 
+        uint256 fee,
+        uint32 source, 
+        uint32 dest
+    );
+    event Debug(string tag, uint256 amount); // TODO
+
     // ============ State Variables ============
     uint32 public constant LOCAL_DOMAIN = 0;
     uint32 public constant MESSAGE_BODY_VERSION = 1;
@@ -16,7 +28,9 @@ contract TokenMessengerWithMetadataTest is Test, TestUtils {
     bytes32 public constant REMOTE_TOKEN_MESSENGER =
         0x00000000000000000000000057d4eaf1091577a6b7d121202afbd2808134f117;
 
-    uint32 public constant ALLOWED_BURN_AMOUNT = 42;
+    address payable public constant COLLECTOR = address(0x1);
+
+    uint32 public constant ALLOWED_BURN_AMOUNT = 42000000;
     MockMintBurnToken public token = new MockMintBurnToken();
     TokenMinter public tokenMinter = new TokenMinter(tokenController);
 
@@ -39,8 +53,12 @@ contract TokenMessengerWithMetadataTest is Test, TestUtils {
         tokenMessengerWrapper = new TokenMessengerWithMetadata(
             address(tokenMessenger),
             REMOTE_DOMAIN,
-            REMOTE_TOKEN_MESSENGER
+            REMOTE_TOKEN_MESSENGER,
+            LOCAL_DOMAIN,
+            COLLECTOR
         );
+
+        tokenMessengerWrapper.setFee(REMOTE_DOMAIN, 0, 0);
 
         tokenMessenger.addLocalMinter(address(tokenMinter));
         tokenMessenger.addRemoteTokenMessenger(
@@ -65,11 +83,70 @@ contract TokenMessengerWithMetadataTest is Test, TestUtils {
         tokenMessengerWrapper = new TokenMessengerWithMetadata(
             address(0),
             REMOTE_DOMAIN,
-            REMOTE_TOKEN_MESSENGER
+            REMOTE_TOKEN_MESSENGER,
+            LOCAL_DOMAIN,
+            COLLECTOR
         );
     }
 
-    function testDepositForBurn_succeeds(
+    function testDepositForBurn_success(
+        uint256 _amount,
+        address _mintRecipient
+    ) public {
+        _amount = 20000000; // $20
+
+        vm.assume(_mintRecipient != address(0));
+        bytes32 _mintRecipientRaw = Message.addressToBytes32(_mintRecipient);
+
+        token.mint(owner, _amount);
+        tokenMessengerWrapper.setFee(REMOTE_DOMAIN, 10, 0); // 10 bips or 0.001%
+
+        vm.prank(owner);
+        token.approve(address(tokenMessengerWrapper), _amount);
+
+        vm.prank(owner);
+        tokenMessengerWrapper.depositForBurn(
+            _amount,
+            REMOTE_DOMAIN,
+            _mintRecipientRaw,
+            address(token),
+            bytes32(0)
+        );
+
+        emit Debug("collector", token.balanceOf(COLLECTOR));
+        assertEq(0, token.balanceOf(owner));
+        assertEq(20000, token.balanceOf(COLLECTOR));
+    }
+
+    function testDepositForBurn_someRemainingTokensSuccess(
+        uint256 _amount,
+        address _mintRecipient
+    ) public {
+        _amount = 20000000; // $20
+
+        vm.assume(_mintRecipient != address(0));
+        bytes32 _mintRecipientRaw = Message.addressToBytes32(_mintRecipient);
+
+        token.mint(owner, _amount);
+        tokenMessengerWrapper.setFee(REMOTE_DOMAIN, 0, 2000000); // $2
+
+        vm.prank(owner);
+        token.approve(address(tokenMessengerWrapper), _amount);
+
+        vm.prank(owner);
+        tokenMessengerWrapper.depositForBurn(
+            10000000,
+            REMOTE_DOMAIN,
+            _mintRecipientRaw,
+            address(token),
+            bytes32(0)
+        );
+
+        assertEq(10000000, token.balanceOf(owner));
+        assertEq(2000000, token.balanceOf(COLLECTOR));
+    }
+
+    function testRawDepositForBurn_succeeds(
         uint256 _amount,
         address _mintRecipient
     ) public {
@@ -80,20 +157,21 @@ contract TokenMessengerWithMetadataTest is Test, TestUtils {
 
         bytes memory metadata = "";
 
-        //
         token.mint(owner, _amount * 2);
 
         vm.prank(owner);
         token.approve(address(tokenMessengerWrapper), _amount);
 
-        //
         vm.prank(owner);
         tokenMessengerWrapper.rawDepositForBurn(
             _amount, _mintRecipientRaw, address(token), metadata
         );
+
+        assertEq(_amount, token.balanceOf(owner));
+        assertEq(0, token.balanceOf(COLLECTOR));
     }
 
-    function testDepositForBurnWithCaller_succeeds(
+    function testRawDepositForBurnWithCaller_succeeds(
         uint256 _amount,
         address _mintRecipient
     ) public {
@@ -104,13 +182,11 @@ contract TokenMessengerWithMetadataTest is Test, TestUtils {
 
         bytes memory metadata = "";
 
-        //
         token.mint(owner, _amount * 2);
 
         vm.prank(owner);
         token.approve(address(tokenMessengerWrapper), _amount);
 
-        //
         vm.prank(owner);
         tokenMessengerWrapper.rawDepositForBurnWithCaller(
             _amount,
@@ -119,5 +195,70 @@ contract TokenMessengerWithMetadataTest is Test, TestUtils {
             destinationCaller,
             metadata
         );
+
+        assertEq(_amount, token.balanceOf(owner));
+        assertEq(0, token.balanceOf(COLLECTOR));
+    }
+
+    function testRawDepositForBurn_flatFee(
+        uint256 _amount,
+        address _mintRecipient
+    ) public {
+        _amount = 20000000; // $20
+
+        vm.assume(_mintRecipient != address(0));
+        bytes32 _mintRecipientRaw = Message.addressToBytes32(_mintRecipient);
+
+        bytes memory metadata = "";
+
+        token.mint(owner, _amount);
+        tokenMessengerWrapper.setFee(REMOTE_DOMAIN, 0, 4000000); // $4
+
+        vm.startPrank(owner);
+        token.approve(address(tokenMessengerWrapper), _amount);
+
+        vm.expectEmit();
+        emit Collect(address(token), _mintRecipientRaw, 20000000, 4000000, LOCAL_DOMAIN, REMOTE_DOMAIN);
+
+        tokenMessengerWrapper.rawDepositForBurn(
+            _amount,
+            _mintRecipientRaw,
+            address(token),
+            metadata
+        );
+        vm.stopPrank();
+
+        assertEq(0, token.balanceOf(owner));
+        assertEq(4000000, token.balanceOf(COLLECTOR));
+    }
+
+    function testRawDepositForBurn_percFee(
+        uint256 _amount,
+        address _mintRecipient
+    ) public {
+        _amount = 20000000; // $20
+
+        vm.assume(_mintRecipient != address(0));
+        bytes32 _mintRecipientRaw = Message.addressToBytes32(_mintRecipient);
+
+        bytes memory metadata = "";
+
+        token.mint(owner, _amount);
+        tokenMessengerWrapper.setFee(REMOTE_DOMAIN, 10, 0); // 10 bips or 0.001%
+
+        vm.prank(owner);
+        token.approve(address(tokenMessengerWrapper), _amount);
+
+        //
+        vm.prank(owner);
+        tokenMessengerWrapper.rawDepositForBurn(
+            _amount,
+            _mintRecipientRaw,
+            address(token),
+            metadata
+        );
+
+        assertEq(0, token.balanceOf(owner));
+        assertEq(_amount / 1000, token.balanceOf(COLLECTOR));
     }
 }
